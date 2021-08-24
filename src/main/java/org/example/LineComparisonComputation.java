2886678af83e68cded562f9ab4aa733203726604
Prashanth <imprash4u@gmail.com>
2021-08-24 20:13:19 +0530

    Added: project Structure

---
+++ src/main/java/org/example/LineComparisonComputation.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class LineComparisonComputation {
+    public static void main(String[] args) {
+        System.out.println("Welocome to Line Comaparison Computation program");
+    }
+}
