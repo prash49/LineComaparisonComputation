68d0f6f96deb9a9ee7301ef40c7347058966ee2d
Prashanth <imprash4u@gmail.com>
2021-08-24 21:21:03 +0530

    Adding:Comparing two lines by taking inputs for (x1 x2 y1 y2) and (x3 x4 y3 y4)

--- src/main/java/com/bridgelabz/LineComparisonComputation.java
+++ src/main/java/com/bridgelabz/LineComparisonComputation.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class LineComparisonComputation {
     public static void main(String[] args) {
-        //Varialbles
-        int x1, x2, y1, y2 ;
+        //Variables
+        int x1, x2, y1, y2 ,x3 , x4, y3 ,y4 ;
         //calling Scanner method to take inputs from console
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter Cartesian Co-ordinates");
@@ -16,6 +16,22 @@ public class LineComparisonComputation {
         y1 = sc.nextInt();
         System.out.println("Enter y2 ");
         y2 = sc.nextInt();
-        System.out.println("Length of the line:"+(Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2)));
+        double line1=(Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2));
+        System.out.println("Length of the line:"+line1);
+        System.out.println("Enter Coordinates of next line : ");
+        System.out.println("Enter x1 of line 2 : ");
+         x3 = sc.nextInt();
+        System.out.println("Enter x2 of line 2 : ");
+         x4 = sc.nextInt();
+        System.out.println("Enter y1 of line 2 : ");
+         y3 = sc.nextInt();
+        System.out.println("Enter y2 of line 2 : ");
+         y4 = sc.nextInt();
+        double line2 =(Math.sqrt(Math.pow((x4-x3),2))+Math.pow((y4-y3) ,2));
+        if (line1 == line2)
+        {
+            System.out.println("Both the lines are equal in length.");
+        }
+        else System.out.println("Both lines are unequal in length.");
     }
 }
