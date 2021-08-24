75b251870e5210a63b82c158cbf5720ee38b4f32
Prashanth <imprash4u@gmail.com>
2021-08-24 21:09:04 +0530

    Added: adding linecomaparison compuation program to check the distance between x and y coordinates

--- src/main/java/com/bridgelabz/LineComparisonComputation.java
+++ src/main/java/com/bridgelabz/LineComparisonComputation.java
@@ -1,7 +1,21 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
 public class LineComparisonComputation {
     public static void main(String[] args) {
-        System.out.println("Welocome to Line Comaparison Computation program");
+        //Varialbles
+        int x1, x2, y1, y2 ;
+        //calling Scanner method to take inputs from console
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Cartesian Co-ordinates");
+        System.out.println("Enter x1 ");
+        x1 = sc.nextInt();
+        System.out.println("Enter x2 ");
+        x2 = sc.nextInt();
+        System.out.println("Enter y1 ");
+        y1 = sc.nextInt();
+        System.out.println("Enter y2 ");
+        y2 = sc.nextInt();
+        System.out.println("Length of the line:"+(Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2)));
     }
 }
